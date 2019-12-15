46623b5010a065a7b85e49e52f3b9e898b18b5a3
Shadewalker1984 <Shadewalker1984@yahoo.com>
2019-12-14 21:13:57 -0600

    fixed the tabs with Chris' help, thank you Chris

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -41,7 +41,7 @@ axios
           user, and adding that card to the DOM.
 */
 
-const followersArray = ['avpimblesr', 'tanveersaleem786', "mtrew2015", "williamschwindt", "scratchglory", "HeyMichelle"];
+// const followersArray = ['avpimblesr', 'tanveersaleem786', "mtrew2015", "williamschwindt", "scratchglory", "HeyMichelle"];
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
@@ -62,69 +62,101 @@ const followersArray = ['avpimblesr', 'tanveersaleem786', "mtrew2015", "williams
 </div>
 
 */
-const cards = document.querySelector('.cards');
+  // const cards = document.querySelector('.cards');
 
 
 
-function createCard(obj) {
-    const card = document.createElement ('div');
-    const img = document.createElement('img');
-    const cardInfo = document.createElement('div');
-    const title = document.createElement('h3');
-    const nameUser = document.createElement('p');
-    const loc = document.createElement('p');
-    const prof = document.createElement('p');
-    const followers = document.createElement('p');
-    const following = document.createElement('p');
-    const bio = document.createElement('p');
 
-
-cards.appendChild(img);
-cards.appendChild(cardInfo);
-cardInfo.appendChild(title);
-cardInfo.appendChild(nameUser);
-cardInfo.appendChild(loc);
-cardInfo.appendChild(prof);
-cardInfo.appendChild(followers);
-cardInfo.appendChild(following);
-cardInfo.appendChild(bio);
-
-cards.classList.add('card');
-cardInfo.classList.add('card-info');
-title.classList.add('name');
-nameUser.classList.add('username');
-
-img.src = obj.avatar_url;
-title.textContent = obj.name;
-nameUser.textContent = obj.login;
-loc.textContent =  "Location: " + obj.location;
-prof.textContent = "Profile: " + obj.html_url;
-followers.textContent = "Followers: " + obj.followers;
-following.textContent = "Following: " + obj.following;
-bio.textContent = "Bio: " + obj.bio;
-
-return cards
-
-}
-
-let nextInLine = followersArray.foreach((data) => {
-  for (let i=0; i<items.length; i++) {
+  const cards = document.querySelector(".cards")
   axios
-    .get('https://api.github.com/users/' + followersArray[i])
-    .then((res1 => {
-      console.log(res1);
-      const followerCard = (res.data);
-    createCard(followerCard);
-    }))
-
-    .catch((err) => {
-      console.log("you hit an error", err);
+    .get("https://api.github.com/users/ShawnBatson")
+    .then(res => {
+      console.log(res)
+      const followCard = res.data
+      cards.appendChild(createCard(followCard))
     })
+    .catch(err => {
+      console.log("you hit an error", err)
+    })
+  const followersArray = [
+    "avpimblesr",
+    "tanveersaleem786",
+    "mtrew2015",
+    "williamschwindt",
+    "scratchglory",
+    "HeyMichelle"
+  ]
+  function createCard(obj) {
+    const card = document.createElement("div")
+    const img = document.createElement("img")
+    const cardInfo = document.createElement("div")
+    const title = document.createElement("h3")
+    const nameUser = document.createElement("p")
+    const loc = document.createElement("p")
+    const prof = document.createElement("p")
+    const followers = document.createElement("p")
+    const following = document.createElement("p")
+    const bio = document.createElement("p")
+    card.appendChild(img)
+    card.appendChild(cardInfo)
+    cardInfo.appendChild(title)
+    cardInfo.appendChild(nameUser)
+    cardInfo.appendChild(loc)
+    cardInfo.appendChild(prof)
+    cardInfo.appendChild(followers)
+    cardInfo.appendChild(following)
+    cardInfo.appendChild(bio)
+    card.classList.add("card")
+    cardInfo.classList.add("card-info")
+    title.classList.add("name")
+    nameUser.classList.add("username")
+    img.src = obj.avatar_url
+    title.textContent = obj.name
+    nameUser.textContent = obj.login
+    loc.textContent = "Location: " + obj.location
+    prof.textContent = "Profile: " + obj.html_url
+    followers.textContent = "Followers: " + obj.followers
+    following.textContent = "Following: " + obj.following
+    bio.textContent = "Bio: " + obj.bio
+    return card
   }
-})
+  followersArray.forEach(follower => {
+    axios
+      .get(`https://api.github.com/users/${follower}`)
+      .then(res1 => {
+        const followerCardA = res1.data
+        cards.appendChild(createCard(followerCardA))
+      })
+      .catch(err => {
+        console.log("you hit an error", err)
+      })
+  })
+ 
+//   const nextInLine = followersArray.forEach(() => {
+//     for (let i = 0; i < followersArray.length; i++ ) {
+//       axios.get(`https://api.github.com/users/${followersArray[i]}`)
+//       .then((res1) => {
+//         const followerCardA = (res1.data);
+//         createCard(followerCardA);
+
+//       })
+    
+//     .catch((err) => {
+//       console.log('you hit an error', err);
+//     })
+//    }
+//   })
+
+
+// console.log(followersArray)
+
+
+
+
+
 
 /* List of LS Instructors Github username's: 
-  tetondan
+  tetondanCh
   dustinmyers
   justsml
   luishrd
